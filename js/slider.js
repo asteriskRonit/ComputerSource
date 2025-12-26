d48672ea023598a59600d8ba201d4c7d533532d2
Ronit Paul <paulronit3280@gmail.com>
2025-12-26 16:15:04 +0530

    first commit

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+const slides = document.querySelectorAll('.hero-slider .slide');
+let idx = 0;
+setInterval(() => {
+  slides[idx].classList.remove('active');
+  idx = (idx + 1) % slides.length;
+  slides[idx].classList.add('active');
+}, 4000);
+
+
